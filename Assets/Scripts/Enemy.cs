d80ea24f818e70856ba309556be6f9beb0086845
Aining Li <annieli2025@u.northwestern.edu>
2023-07-21 11:19:50 +0800

    bullet can collide with enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,10 +12,24 @@ public class Enemy : MonoBehaviour
     {
         pathFinder = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
     }
     
     void Update()
     {
-        pathFinder.SetDestination(target.position);
+        
+    }
+
+    // put 'SetDestination' this way so that it's updating 4 time per second, compared to updating every frame(could be 60 times per second) if putting it in Update method. This improves game performance.
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;
+        while(target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathFinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
     }
 }
