a589d9a0abeeabf072c1e6cafc85cad7cf7e5a07
Aining Li <annieli2025@u.northwestern.edu>
2023-07-27 18:18:10 +0800

    add menu functionality (unfinished)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mainMenuHolder;
+    public GameObject optionsMenuHolder;
+
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public int[] screenWidths;
+    int activeScreenResIndex;
+
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+    }
+
+    public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+            //PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            //PlayerPrefs.Save();
+        }
+    }
+    public void SetFullScreen(bool isFullscreen)
+    {
+        // set it to max resolution when full screen to go around a bug (according to the video)
+        for(int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullscreen;
+        }
         
+        if(isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
+
+    }
+    public void SetMasterVolume(float value)
+    {
+
+    }
+    public void SetMusicVolume(float value)
+    {
+
+    }
+    public void SetSfxVolume(float value)
+    {
+
     }
 }
