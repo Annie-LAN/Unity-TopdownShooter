49298e828073840fb9bb5a74b8dbbe18c7935e2a
Aining Li <annieli2025@u.northwestern.edu>
2023-07-28 14:57:35 +0800

    add menu functionality

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,8 +12,26 @@ public class Menu : MonoBehaviour
 
     public Slider[] volumeSliders;
     public Toggle[] resolutionToggles;
+    public Toggle fullscreenToggle;
     public int[] screenWidths;
     int activeScreenResIndex;
+
+    void Start()
+    {
+        activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
+        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") ==1) ? true: false;
+
+        volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
+        volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
+        volumeSliders[2].value = AudioManager.instance.sfxVolumePercent;
+
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].isOn = i == activeScreenResIndex;
+        }
+
+       fullscreenToggle.isOn = isFullscreen;
+    }
 
     public void Play()
     {
@@ -43,8 +62,8 @@ public class Menu : MonoBehaviour
             activeScreenResIndex = i;
             float aspectRatio = 16 / 9f;
             Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
-            //PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
-            //PlayerPrefs.Save();
+            PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            PlayerPrefs.Save();
         }
     }
     public void SetFullScreen(bool isFullscreen)
@@ -65,18 +84,19 @@ public class Menu : MonoBehaviour
         {
             SetScreenResolution(activeScreenResIndex);
         }
-
+        PlayerPrefs.SetInt("fullscreen", ((isFullscreen) ? 1 : 0));
+        PlayerPrefs.Save();
     }
     public void SetMasterVolume(float value)
     {
-
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
     }
     public void SetMusicVolume(float value)
     {
-
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
     }
     public void SetSfxVolume(float value)
     {
-
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
     }
 }
