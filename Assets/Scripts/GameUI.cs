3527f3929d2e1c544600eb0f74565697a3d40d23
Aining Li <annieli2025@u.northwestern.edu>
2023-07-28 18:00:23 +0800

    add player health bar

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -14,11 +14,14 @@ public class GameUI : MonoBehaviour
     public TextMeshProUGUI newWaveTitle;
     public TextMeshProUGUI newWaveEnemyCount;
     public TextMeshProUGUI scoreUI;
+    public RectTransform healthBar;
 
     Spawner spawner;
+    Player player;
     void Start()
     {
-        FindObjectOfType<Player>().OnDeath += OnGameOver;
+        player = FindObjectOfType<Player>();
+        player.OnDeath += OnGameOver;
     }
 
     void Awake()
@@ -29,6 +32,12 @@ public class GameUI : MonoBehaviour
     void Update()
     {
         scoreUI.text = ScoreKeeper.score.ToString("D6");
+        float healthPercent = 0;
+        if(player != null)
+        {
+            healthPercent = player.health / player.startingHealth;            
+        }
+        healthBar.localScale = new Vector3(healthPercent, 1, 1);
     }
 
     void OnNewWave(int waveNumber)
